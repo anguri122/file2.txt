e70c8656c64a8e9896fabe46412c37518fdebc38
Chaitanya Anguri <anguri2264@gmail.com>
2024-11-26 07:05:26 +0530

    first commit

--- 1.py
+++ 1.py
@@ -1,23 +1,36 @@
-
-
-
 def calculator():
-        number1=float(input('enter a number:'))
-        number2=float(input('enter a number:'))
-        print('''chose the value where\n 1==addition\n 2==substration\n 3==multplication\nand 4==division\n''')
-        def switch(value):
+    number1 = float(input("Enter the first number: "))
+    number2 = float(input("Enter the second number: "))
     
-            if value ==1:
-                return number1+number2
-            elif value ==2:
-                return number1-number2
-            elif value ==3:
-                return number1*number2
-            elif value ==2:
-                return number1/number2
-        c=switch(float(input('enter a value:')))
-        print('resultis:',c)
+    print('''
+Choose an operation:
+1 == Addition
+2 == Subtraction
+3 == Multiplication
+4 == Division
+    ''')
+    
+    def switch(value):
+        if value == 1:
+            return number1 + number2
+        elif value == 2:
+            return number1 - number2
+        elif value == 3:
+            return number1 * number2
+        elif value == 4:
+            if number2 != 0:  # Avoid division by zero
+                return number1 / number2
+            else:
+                return "Error: Division by zero is undefined."
+        else:
+            return "Error: Invalid operation choice."
+    
+    try:
+        c = switch(int(input("Enter your choice (1-4): ")))
+        print("Result is:", c)
+    except ValueError:
+        print("Error: Please enter a valid number for the operation choice.")
+
 calculator()
- 
 
         
